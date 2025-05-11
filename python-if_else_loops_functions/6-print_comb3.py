426d0da4a52ad79e96cde89513892de1a04b1106
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-05-11 09:43:41 +0000

    fix code style 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i != 8 or j != 9:
-            print("{0:d}{1:d}, ".format(i,j), end="")
+            print("{0:d}{1:d}, ".format(i, j), end="")
         else:
-            print("{0:d}{1:d}".format(i,j))
+            print("{0:d}{1:d}".format(i, j))
