0fc8046be582c1e83c63dd8e988854379c5a5f52
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-05-13 12:32:13 +0000

    add 1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx >= 0 and idx < len(my_list):
-        return my_list[idx]
+def replace_in_list(my_list, idx, element):
+    if idx >=0 and idx < len(my_list):
+        my_list[idx] = element
+        return my_list
     return None
