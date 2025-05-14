0121c0bc1f5e713e79456dc8bbb6ca7a3f4ad878
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-05-14 11:39:34 +0300

    fix the output 6-print_sorted_dictionary.py

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    print(sorted(a_dictionary.keys()))
+    dic_keys = sorted(a_dictionary.keys())
+    for key in dic_keys:
+        print("{0}: {1}".format(key, a_dictionary[key]))
