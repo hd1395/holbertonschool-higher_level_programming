6e8c607398fa720d9b462c745fb860234885594c
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-05-14 14:16:51 +0300

    Fix None dictionary 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
+    if a_dictionary == None or len(a_dictionary) == 0:
         return None
     return list(a_dictionary.keys())[list(a_dictionary.values()).index(sorted(a_dictionary.values())[-1])]
