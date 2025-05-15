293cd750e6a5c5a3e7bfb4b1a7990a55b99c19bf
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-05-15 15:01:05 +0300

    add 0-safe_print_list.py

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    error = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end = "")
+        print()
+    except IndexError:
+        i -= 1
+    return i + 1
