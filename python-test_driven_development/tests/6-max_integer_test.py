5ef32ee64e270438d7de33da28764e4b3fa25e0f
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-05-18 23:06:12 +0300

    add 6-max_integer_test.py

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+
+    def test_max_integer(self):
+        self.assertEqual(max_integer([13, 42, 73, 54]), 73)
+        self.assertEqual(max_integer([5, 5, 5, 5]), 5)
+        self.assertEqual(max_integer([-89, 0, -20, -40]), 0)
+        self.assertEqual(max_integer([50]), 50)
+        self.assertEqual(max_integer([]), None)
+
+    def test_max_boolean(self):
+        self.assertTrue(max_integer([True, False]), True)
+
+    def test_max_string(self):
+        self.assertEqual(max_integer(["tuwaiq"]), "tuwaiq")
