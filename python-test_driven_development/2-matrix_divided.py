90f162da99b289896cfa7aa0fa43057cbdd7ffd4
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-05-18 22:37:36 +0300

    fix code style 2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -23,26 +23,27 @@ def matrix_divided(matrix, div):
     """
     row_len = 0
     size_error_msg = "Each row of the matrix must have the same size"
-    type_error_msg = "matrix must be a matrix (list of lists) of integers/floats"
+    type_err_msg = "matrix must be a matrix (list of lists) of integers/floats"
     if not matrix or not isinstance(matrix, list):
-        raise TypeError(type_error_msg)
+        raise TypeError(type_err_msg)
 
     for row in matrix:
         if not row or not isinstance(row, list):
-            raise TypeError(type_error_msg)
+            raise TypeError(type_err_msg)
         if row_len != 0 and len(row) != row_len:
             raise TypeError(size_error_msg)
 
         for num in row:
             if not isinstance(num, (float, int)):
-                raise TypeError(type_error_msg)
+                raise TypeError(type_err_msg)
         row_len = len(row)
-    
+
     if not isinstance(div, (float, int)):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_list = list(map(lambda row: list(map(lambda n: round(n / div, 2), row)), matrix))
+    new_list = list(map(lambda row: list(map(lambda n: round(n / div, 2), row))
+                        , matrix))
     return (new_list)
