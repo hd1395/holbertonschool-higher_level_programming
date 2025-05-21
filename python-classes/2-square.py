ff6fc703ba522a209cd21d72f666f446c2250e93
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-05-21 10:17:36 +0300

    fix error 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-This module defines a Square class with size validation.
+This module defines a Square class with attribute size validation.
 """
 
 
 class Square:
     """
-    Represents a square with a private size attribute.
+    Square class with a private size attribute.
 
     Attributes:
         __size (int): The size of the square (private).
