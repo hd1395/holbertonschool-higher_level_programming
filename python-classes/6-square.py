b6d82e6966293ba3eb517a274bfbdaa4f6a269a7
Hamad Dahash <hdahash@hotmail.com>
2025-05-22 06:31:39 +0300

    fix code style 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -19,7 +19,7 @@ class Square:
         __size (int): The size of the square (private).
     """
 
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         """
         Initializes a new Square instance with a given size.
 
