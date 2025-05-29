1f6663e15ba9b8dadd8ecfe4d8bac94e68b5b289
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-05-29 15:12:22 +0300

    add 4-inherits_from.py

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Module for inherits_from method.'''
+
+
+def inherits_from(obj, a_class):
+    '''object is an instance of a class that inherited 
+    (directly or indirectly) from the specified class
+
+    arguments:
+        obj(object) whose class is to be inspected
+        a_class(class) the class to be inspected
+    Return: True if the class of the object is a subclass of the specified
+            class, otherwise false
+    '''
+    return isinstance(obj, a_class) and type(obj) is not a_class
