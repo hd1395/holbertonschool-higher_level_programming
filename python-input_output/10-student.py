8506a060262655215b279e8afe05d99c412d6df7
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-06-11 11:46:47 +0300

    add 10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represents a student class."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Gets a dictionary representation of the Student.
+        If attrs is a list of strings, only attribute names contained in
+        this list must be retrieved.
+        Otherwise, all attributes must be retrieved.
+
+        Returns:
+            dict: dictionary representation.
+        """
+        if attrs is None:
+            return self.__dict__
+
+        result_dict = {}
+        for attr in attrs:
+            try:
+                result_dict[attr] = self.__dict__[attr]
+            except Exception:
+                pass
+        return result_dict
