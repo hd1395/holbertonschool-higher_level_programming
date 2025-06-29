744a9c28d7df6cf493e16368efe70bd436b21a03
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-06-29 10:18:29 +0300

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -18,8 +18,11 @@ def list_states(username, password, dbname):
         dbname (str): database name
     """
     db = MySQLdb.connect(
-        host="localhost", port=3306, user=username, passwd=password, db=dbname
-    )
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=dbname)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cursor.fetchall()
