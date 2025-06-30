db2c9d9a31bfaf1a5f255079e3138998a1ad758b
Hamad <hdahash@hotmail.com>
2025-06-30 08:51:40 +0000

    fix 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,23 @@
 #!/usr/bin/python3
-"""
-Script that lists all states from the database hbtn_0e_0_usa.
-
-Connects to MySQL server using MySQLdb and retrieves states
-ordered by their id in ascending order.
-"""
+""" Lists all states from the database hbtn_0e_0_usa """
 
 import MySQLdb
-import sys
-
-
-def list_states(username, password, dbname):
-    """
-    Connects to MySQL database and prints all states sorted by id.
-
-    Args:
-        username (str): MySQL username
-        password (str): MySQL password
-        dbname (str): database name
-    """
-    db = MySQLdb.connect(
-        host="localhost", port=3306, user=username, passwd=password, db=dbname
-    )
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close()
-    db.close()
-
+from sys import argv
 
 if __name__ == "__main__":
-    list_states(sys.argv[1], sys.argv[2], sys.argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8"
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
