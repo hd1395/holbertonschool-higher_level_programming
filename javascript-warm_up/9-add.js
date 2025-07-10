95f447172b948539634c9beff62a7d5fbf0c25c7
hamad <hamadsocial75@gmail.com>
2025-07-10 20:57:20 +0300

    fix 9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -6,8 +6,6 @@ function add (a, b) {
 const num1 = parseInt(process.argv[2], 10);
 const num2 = parseInt(process.argv[3], 10);
 
-if (isNaN(num1) || isNaN(num2)) {
-  console.log('Arguments must be integers');
-} else {
-  console.log(add(num1, num2));
-}
+const a = parseInt(process.argv[2], 10);
+const b = parseInt(process.argv[3], 10);
+console.log(add(a, b));
