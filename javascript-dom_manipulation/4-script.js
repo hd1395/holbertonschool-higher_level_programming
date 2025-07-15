e8d6cbe945b9cb536864c42303545ed14e366eaa
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-07-15 11:56:32 +0300

    add 4-script.js

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+document.querySelector('#add_item').addEventListener('click', function () {
+  const newItem = document.createElement('li');
+  newItem.textContent = 'Item';
+  document.querySelector('ul.my_list').appendChild(newItem);
+});
