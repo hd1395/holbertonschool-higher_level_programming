5f3e5679e2972775e0d9b678b633d00f89e79a99
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-07-15 12:18:23 +0300

    add 8-script.js

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      document.querySelector("#hello").textContent = data.hello;
+    });
