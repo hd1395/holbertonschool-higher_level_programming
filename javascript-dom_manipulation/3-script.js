fddad9ffa5d4bf94a8ec6165769e6fc044010e9b
Hamad <hdahash@hotmail.com>
2025-07-15 12:20:52 +0000

    make files executable and fix the code by semistandard

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 document.querySelector('#toggle_header').addEventListener('click', function () {
   const header = document.querySelector('header');
-  
+
   if (header.classList.contains('red')) {
     header.classList.replace('red', 'green');
   } else {
--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -3,11 +3,11 @@ const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
 fetch(url)
   .then(response => {
     if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status}`);
-  }
-  return response.json();
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
   })
   .then(data => {
     const characterName = data.name;
-    document.querySelector("#character").textContent = characterName;
-  })
+    document.querySelector('#character').textContent = characterName;
+  });
