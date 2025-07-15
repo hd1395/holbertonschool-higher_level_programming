ea8238697cdba395f5706f4c84e0a562da0016df
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-07-15 12:15:02 +0300

    add 7-script.js

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+  const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+  fetch(url)
+    .then(response => {
+      if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+      return response.json();
+    })
+    .then(data => {
+      document.querySelector('#list_movies').innerHTML = '';
+
+      data.results.forEach(movie => {
+        const li = document.createElement('li');
+        li.textContent = movie.title;
+        document.querySelector('#list_movies').appendChild(li);
+      });
+    })
